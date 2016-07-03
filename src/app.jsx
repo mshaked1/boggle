1de5d8c2a05c974937d294cdeab8d7994a145861
codesmith <student@mac-mini-9.local>
2016-07-02 17:19:14 -0700

    can't get componentDidMount win cases to function, need to bring all logic and functions to Board component

--- src/app.jsx
+++ src/app.jsx
@@ -23,15 +23,17 @@ var Board = React.createClass({
     return {
 
       boardObj: {'00': '-', '01': '-', '02': '-', '10': '-', '11': '-', '12': '-', '20': '-', '21': '-', '22': '-' },
-      counter: 0
+      letter: 'O',
+      turns: 0,
     }
   },
-  
-  handleSwitchBox: function(value, targetRow, targetBox, counter){
-    console.log(targetRow+targetBox);
+
+  handleSwitchBox: function(target, letter, turns){
     const newState = this.state;
-    newState.boardObj[targetRow+targetBox] = value;
-    newState.counter = counter;
+    newState.boardObj[target] = letter;
+    newState.letter = letter;
+    newState.turns = turns;
+    console.log("newState turns "+newState.turns);
     this.setState({newState});
   },
 
@@ -42,21 +44,51 @@ var Board = React.createClass({
   render: function(){
     var board = [];
     for (var rows = 0; rows < 3; rows++){
-      board.push(<Row handleSwitchBox1={this.handleSwitchBox} key={rows} rowID={rows} boardProp={this.state.boardObj} counter={this.state.counter}/>);
+      board.push(<Row handleSwitchBox1={this.handleSwitchBox} key={rows} id={rows} boardProp={this.state.boardObj} letter={this.state.letter} turns={this.state.turns}/>);
     }
     return (<div>{board}</div>)
+  },
+
+  componentDidUpdate: function(){
+    const boardSpot = this.state.boardObj;
+    const totalTurns = this.state.turns;
+    console.log(totalTurns);
+    if( (totalTurns > 3) && (  (boardSpot['00'] === boardSpot['10']) && (boardSpot['00'] === boardSpot['20'])))   {
+      alert(this.state.letter+" wins!");
+    }
+    if( (totalTurns > 3) && ( (boardSpot['00'] === boardSpot['01']) && (boardSpot['00'] === boardSpot['02']) )){
+      alert(this.state.letter+" wins!");
+    }
+    if( (totalTurns > 3) && (  (boardSpot['00'] === boardSpot['11']) && (boardSpot['00'] === boardSpot['22'])) ){
+      alert(this.state.letter+" wins!");
+    }
+    if( (totalTurns > 3) && (  (boardSpot['10'] === boardSpot['11']) && (boardSpot['11'] === boardSpot['12'])) ){
+      alert(this.state.letter+" wins!");
+    }
+    if( (totalTurns > 3) && (  (boardSpot['20'] === boardSpot['21']) && (boardSpot['20'] === boardSpot['22'])) ){
+      alert(this.state.letter+" wins!");
+    }
+    if( (totalTurns > 3) && (  (boardSpot['20'] === boardSpot['11']) && (boardSpot['20'] === boardSpot['02'])) ){
+      alert(this.state.letter+" wins!");
+    }
+    if( (totalTurns > 3) && (  (boardSpot['01'] === boardSpot['11']) && (boardSpot['01'] === boardSpot['21'])) ){
+      alert(this.state.letter+" wins!");
+    }
+    if( (totalTurns > 3) && (  (boardSpot['02'] === boardSpot['12']) && (boardSpot['02'] === boardSpot['22']))) {
+      alert(this.state.letter+" wins!");
+    }
   }
 })
 
 var Row = React.createClass({
 
-
   render: function() {
     var ticTacRow = [];
     for (var box = 0; box < 3; box++) {
      
-      ticTacRow.push(<Box handleSwitchBox2={this.props.handleSwitchBox1} row={this.props.rowID} key={box} boxID={box} boardProp1={this.props.boardProp} counter={this.props.counter}/>);
+      ticTacRow.push(<Box handleSwitchBox2={this.props.handleSwitchBox1} id={this.props.id.toString() + box} key={box} boardProp1={this.props.boardProp} letter={this.props.letter} turns={this.props.turns}/>);
     }
+    
     return (<div>{ticTacRow}</div>);
     // return (<div>{ this.state }</div>);
   }
@@ -65,19 +97,19 @@ var Row = React.createClass({
 var Box = React.createClass({
 
   switchBox: function(){
-    var targetRow = this.props.row.toString();
-    var targetBox = this.props.boxID.toString();
-    var letter;
-    var counter = this.props.counter;
-    if ( (this.props.boardProp1[targetRow+targetBox] === '-') && (counter === 0) ){
+    var turns = this.props.turns;
+    var target = this.props.id.toString();
+    var letter = this.props.letter;
+    if ( (this.props.boardProp1[target] === '-') && (letter === 'O') ){
+      turns++;
+      console.log("turns "+turns);
       letter = 'X';
-      counter++;
-      this.props.handleSwitchBox2(letter, targetRow, targetBox, counter);
+      this.props.handleSwitchBox2(target, letter, turns);
     }
-    else if( (this.props.boardProp1[targetRow+targetBox] === '-') && (counter === 1) ){
+    else if( (this.props.boardProp1[target] === '-') && (letter === 'X') ){
+      turns++;
       letter = 'O';
-      counter--;
-      this.props.handleSwitchBox2(letter, targetRow, targetBox, counter);
+      this.props.handleSwitchBox2(target, letter, turns);
     }
   },
 
@@ -86,7 +118,7 @@ var Box = React.createClass({
   },
 
   render: function() {
-    return (<button onClick={this.switchBox} style={boxStyle}>{this.props.boardProp1[this.props.row.toString()+this.props.boxID.toString()]}</button>);
+    return (<button style={'color: green'} onClick={this.switchBox} style={boxStyle}>{this.props.boardProp1[this.props.id.toString()]}</button>);
   }
 })
 
